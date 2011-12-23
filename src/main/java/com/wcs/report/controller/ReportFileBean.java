00c4fa4d7a57676a22a654e42e2fcdd3813c5e45
chenlong <chenlong@wcs-global.com>
2011-12-23 07:24:42 +0000

    resolve issue #1882 Tab页面文件上传实现

--- src/main/java/com/wcs/report/controller/ReportFileBean.java
+++ src/main/java/com/wcs/report/controller/ReportFileBean.java
@@ -68,9 +68,21 @@ public class ReportFileBean extends ViewBaseBean<ReportFile> {
         }
     }
 
+    /**
+     * 
+     * <p>Description: 上传模板之前,生产版本号取用户信息</p>
+     */
     public void befforeAdd() {
         this.setInstance(new ReportFile());
         getInstance().setUpLoadedBy((String) JSFUtils.getSession().get("userName"));
+        String str = JSFUtils.getRequestParam("master");
+        if (str != null && !"null".equals(str)) {
+            mastrId = Long.parseLong(str);
+            version = reportFileService.findReptFileNumber(mastrId) + 1;
+        } else {
+            version = reportFileService.findReptFileNumber(mastrId) + 1;
+        }
+
     }
 
     /**
@@ -108,33 +120,33 @@ public class ReportFileBean extends ViewBaseBean<ReportFile> {
      */
     public void update() {
         try {
-            this.reportFileService.updateRptFile(getInstance(),this.mastrId);
+            this.reportFileService.updateRptFile(getInstance(), this.mastrId);
             // 上传的模板设置了为使用 其他模板属性更新为不使用
-          /*  if (this.getInstance().getUseInd()) {
-                this.reportFileService.updateUseInd(getInstance().getId(),this.mastrId);
-            }*/
+            /*
+             * if (this.getInstance().getUseInd()) { this.reportFileService.updateUseInd(getInstance().getId(),this.mastrId); }
+             */
             JSFUtils.getRequest().setAttribute("rptManagerFlag", 1);
             findRptTableData(this.mastrId);
         } catch (Exception e) {
             e.printStackTrace();
         }
-       // return "/faces/report/reportmanage/list.xhtml";
+        // return "/faces/report/reportmanage/list.xhtml";
     }
 
     public void findRptTableData(Long masId) {
         reportFileModel = this.reportFileService.findRptFileDataModel(masId);
         this.setReportFileModel(reportFileModel);
     }
-    
-    public void onTabChange(TabChangeEvent event){
-        Object id  = JSFUtils.getRequestParam("masId");
+
+    public void onTabChange(TabChangeEvent event) {
+        Object id = JSFUtils.getRequestParam("masId");
         System.out.println(JSFUtils.getRequestParam("masId"));
         String title = event.getTab().getTitle();
-        if("报表文件".equals(title) && id  != null && !"null".equals(id)){
-            reportFileModel = this.reportFileService.findRptFileDataModel(Long.parseLong((String)id));
+        if ("报表文件".equals(title) && id != null && !"null".equals(id)) {
+            reportFileModel = this.reportFileService.findRptFileDataModel(Long.parseLong((String) id));
         }
     }
-    
+
     private int validatorFile() throws Exception {
         if (rptFile == null || rptFile.getSize() == 0) {
             JSFUtils.getRequest().setAttribute("rptflag", 1);
@@ -152,7 +164,6 @@ public class ReportFileBean extends ViewBaseBean<ReportFile> {
         return 2;
     }
 
-
     /**
      * 
      * <p>Description:填充报表文件对象 </p>
@@ -164,7 +175,7 @@ public class ReportFileBean extends ViewBaseBean<ReportFile> {
         String fileName = rptCode.concat("_").concat(String.valueOf(version)).concat("_").concat(rptFile.getFileName());
         File file = createRptFile(rptCode, fileName);
         getInstance().setFileName(fileName);
-        getInstance().setFileStoreLocation(FileUtil.FLODER_NAME + File.separator +rptCode+File.separator+ fileName);
+        getInstance().setFileStoreLocation(FileUtil.FLODER_NAME + File.separator + rptCode + File.separator + fileName);
         getInstance().setReportMstr(rptMstr);
         getInstance().setVersionNo(version);
         getInstance().setUpLoadedDatetime(new Date());
@@ -215,9 +226,7 @@ public class ReportFileBean extends ViewBaseBean<ReportFile> {
 
     public void setMastrId(Long mastrId) {
         this.mastrId = mastrId;
-        if (mastrId != null) {
-            version = reportFileService.findReptFileNumber(mastrId) + 1;
-        }
+
     }
 
     public UploadedFile getRptFile() {
@@ -252,5 +261,5 @@ public class ReportFileBean extends ViewBaseBean<ReportFile> {
     public void setReportFileModel(LazyDataModel<ReportFile> reportFileModel) {
         this.reportFileModel = reportFileModel;
     }
-    
+
 }
