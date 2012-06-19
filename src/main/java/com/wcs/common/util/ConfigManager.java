11d7b9e3ae1bc51aec689b188da8a68132a7f50b
hujianguang <hujianguang@wcs-global.com>
2012-06-19 02:59:40 +0000

    配置文件读取方式修改

--- src/main/java/com/wcs/common/util/ConfigManager.java
+++ src/main/java/com/wcs/common/util/ConfigManager.java
@@ -4,6 +4,10 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.lang.System.out;
 
 /**
  * <p>Project: BTC</p>
@@ -15,66 +19,94 @@ import java.util.Properties;
  */
 public final class ConfigManager {
 
-	
-	/** The CONFI g_ file. */
-	private static String CONFIG_FILE = "default.properties";
 
-	/** The config props. */
-	private static Properties configProps = new Properties();
-	
-	/**
-	 * Instantiates a new propertie utils.
-	 */
-	private ConfigManager(){
-		
-	}
-	
-	static{
-		try {
-			configProps.load(ConfigManager.class.getClassLoader().getResourceAsStream(CONFIG_FILE));
-		} catch (Exception e) {
-		}
-	}
-	
+    /**
+     * The CONFI g_ file.
+     */
+    private static final String CONFIG_FILE = "default.properties";
 
-	/**
-	 * Gets the config value.
-	 *
-	 * @param configKey the config key
-	 * @return the config value
-	 */
-	public static String getConfigValue(String configKey) {
-		String configValue = (String) configProps.getProperty(configKey).trim();
-		return configValue;
-	}
-	
-	public static Map<String, String> getConfigValueMapByFilter(String configKeyFilter) {
-		Map<String, String> attrIds = new HashMap<String, String>();
-		String attrId = "";
-		Iterator itr = configProps.entrySet().iterator();
-		while (itr.hasNext()) {
-			Map.Entry e = (Map.Entry) itr.next();
-			attrId = String.valueOf(e.getValue()).trim();
-			if(String.valueOf(e.getKey()).indexOf(configKeyFilter) == 0){
-				attrIds.put(String.valueOf(e.getKey()).replace(configKeyFilter, "").trim(), String.valueOf(e.getValue()).trim());
-			}
-		}
-		return attrIds;
-	}
-	
-	public static String[] getConfigValueArrayByFilter(String configKeyFilter) {
-		String[] attrIds = null;
-		Map<String, String> attrMap = ConfigManager.getConfigValueMapByFilter(configKeyFilter);
-		Iterator itr = attrMap.entrySet().iterator();
-		int i=0;
-		if(attrMap.size() > 0){
-			attrIds = new String[attrMap.size()];
-			while (itr.hasNext()) {
-				Map.Entry e = (Map.Entry) itr.next();
-				attrIds[i] = String.valueOf(e.getValue()).trim();
-				i++;
-			}
-		}
-		return attrIds;
-	}
+    private static final String P_STR = "\\{(.+?)\\}";
+
+    /**
+     * The config props.
+     */
+    private static Properties configProps = new Properties();
+
+    /**
+     * Instantiates a new propertie utils.
+     */
+    private ConfigManager() {
+
+    }
+
+    static {
+        try {
+            configProps.load(ConfigManager.class.getClassLoader().getResourceAsStream(CONFIG_FILE));
+        } catch (Exception e) {
+        }
+    }
+
+    public static Map<String, String> getConfigValueMapByFilter(String configKeyFilter) {
+        Map<String, String> attrIds = new HashMap<String, String>();
+        String attrId = "";
+        Iterator itr = configProps.entrySet().iterator();
+        while (itr.hasNext()) {
+            Map.Entry e = (Map.Entry) itr.next();
+            attrId = String.valueOf(e.getValue()).trim();
+            if (String.valueOf(e.getKey()).indexOf(configKeyFilter) == 0) {
+                attrIds.put(String.valueOf(e.getKey()).replace(configKeyFilter, "").trim(), replaceRealValue(String.valueOf(e.getValue()).trim()));
+            }
+        }
+        return attrIds;
+    }
+
+    public static String[] getConfigValueArrayByFilter(String configKeyFilter) {
+        String[] attrIds = null;
+        Map<String, String> attrMap = ConfigManager.getConfigValueMapByFilter(configKeyFilter);
+        Iterator itr = attrMap.entrySet().iterator();
+        int i = 0;
+        if (attrMap.size() > 0) {
+            attrIds = new String[attrMap.size()];
+            while (itr.hasNext()) {
+                Map.Entry e = (Map.Entry) itr.next();
+                attrIds[i] = replaceRealValue(String.valueOf(e.getValue()).trim());
+                i++;
+            }
+        }
+        return attrIds;
+    }
+
+    /**
+     * <p>这里利用了递归方式</p>
+     * @param key
+     * @return
+     */
+    public static String getValue(String key) {
+        return replaceRealValue(configProps.getProperty(key));
+    }
+
+    /**
+     * <p>同上方法，利用了递归方式获取参数值,这里的{}内的参数必须在配置文件中。</p>
+     * @param srcStr
+     * @return
+     */
+    private static String replaceRealValue(String srcStr) {
+
+        String finalStr = new String(srcStr);
+        Matcher m = Pattern.compile(P_STR).matcher(srcStr);
+
+        while (m.find()) {
+            int i = m.start();
+            int j = m.end();
+
+            String replaceStr = srcStr.substring(i + 1, j - 1);
+
+            finalStr = finalStr.replace("{" + replaceStr + "}", getValue(replaceStr));
+
+        }
+
+        return finalStr;
+
+    }
+
 }
