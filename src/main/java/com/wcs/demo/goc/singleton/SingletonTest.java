0096d87248b6d5355dc752c0ba434ec306bbbe38
袁振才 <yuanzhencai@wcs-global.com>
2014-05-08 13:01:52 +0800

    update:sigleton

---
+++ src/main/java/com/wcs/demo/goc/singleton/SingletonTest.java
@@ -0,0 +1,16 @@
+package com.wcs.demo.goc.singleton;
+
+import org.junit.Test;
+
+public class SingletonTest {
+
+	@Test
+	public void testMode() {
+		Singleton.getInstance().mode();
+		Singleton1.getInstance().mode();
+		Singleton2.getInstance().mode();
+		Singleton3.getInstance().mode();
+		Singleton4.getInstance().mode();
+	}
+
+}
