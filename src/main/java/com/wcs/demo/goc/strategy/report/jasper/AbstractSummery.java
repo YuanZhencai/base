96874fc712bb0372abe1e6eb796dcf692d59c96f
袁振才 <yuanzhencai@wcs-global.com>
2014-05-08 17:55:11 +0800

    strategy:jasper report

---
+++ src/main/java/com/wcs/demo/goc/strategy/report/jasper/AbstractSummery.java
@@ -0,0 +1,18 @@
+/** * AbstractSummery.java 
+* Created on 2014年5月8日 下午5:51:47 
+*/
+
+package com.wcs.demo.goc.strategy.report.jasper;
+
+/** 
+ * <p>Project: btcbase</p> 
+ * <p>Title: AbstractSummery.java</p> 
+ * <p>Description: </p> 
+ * <p>Copyright (c) 2014 Wilmar Consultancy Services</p>
+ * <p>All Rights Reserved.</p>
+ * @author <a href="mailto:yuanzhencai@wcs-global.com">Yuan</a>
+ */
+
+public abstract class AbstractSummery {
+
+}
