777317c1be501fa17ff522de6e78728f02e526d1
Yuan Zhencai <yuanzhencai@wcs-global.com>
2014-06-17 11:22:41 +0800

    comparator

---
+++ src/main/java/com/wcs/demo/ComparatorTest.java
@@ -0,0 +1,56 @@
+/** * ComparatorTest.java 
+ * Created on 2014年6月17日 上午11:09:02 
+ */
+
+package com.wcs.demo;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import org.junit.Test;
+
+/**
+ * <p>
+ * Project: btcbase
+ * </p>
+ * <p>
+ * Title: ComparatorTest.java
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * <p>
+ * Copyright (c) 2014 Wilmar Consultancy Services
+ * </p>
+ * <p>
+ * All Rights Reserved.
+ * </p>
+ * 
+ * @author <a href="mailto:yuanzhencai@wcs-global.com">Yuan</a>
+ */
+
+public class ComparatorTest {
+
+	@Test
+	public void test() {
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(4);
+		list.add(2);
+		list.add(1);
+		list.add(3);
+		Collections.sort(list, new Comparator<Integer>() {
+			public int compare(Integer i1, Integer i2) {
+				return i1 - i2;
+			}
+		});
+		
+		for (Integer i : list) {
+			System.out.println("[i]" + i);
+		}
+	}
+
+}
